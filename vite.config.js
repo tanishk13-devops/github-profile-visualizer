e73f695026c663b4ea74d4ba45b4482e55dfd5c4
Tanishk Jaiswal <tanishk13.devops@gmail.com>
2026-06-02 01:46:34 +0530

    Configure deployment workflow and dynamic base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? '/github-profile-visualizer/' : '/',
   server: {
     port: 5173,
     open: true
